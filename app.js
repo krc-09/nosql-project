7f34eb9b276c2b0266f219d34238011e479707e9
Kankana Roychowdhury <kankanarc2020@gmail.com>
2025-01-11 18:41:29 +0530

    built relation between cart and user

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('67821d970e8baf1c677b4994')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name,user.email,user.cart,user._id);
       next();
     })
     .catch(err => console.log(err));
