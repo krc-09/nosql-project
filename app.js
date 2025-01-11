7e8f209299e9ee3aab5d87c1a0382a7f0b8d6599
Kankana Roychowdhury <kankanarc2020@gmail.com>
2025-01-11 13:17:08 +0530

    added users in db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const sequelize = require('sequelize');
 const bodyParser = require('body-parser');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 
 const errorController = require('./controllers/error');
@@ -21,14 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-//   User.findById(1)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
+  User.findById('67821d970e8baf1c677b4994')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 
-next();
+
  });
 
  app.use('/admin', adminRoutes);
